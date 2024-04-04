c2f9bb4634e2f50059baa36b11218a056db7f1e3
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-04-04 00:02:08 -0700

    print square

---
+++ 0x00-challenge/1-print_square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+ 	prints square with the size passed to iy
+	using hashtags
+*/
+
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+
+size = parseInt(process.argv[2], 10)
+
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
